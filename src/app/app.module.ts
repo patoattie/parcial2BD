88f958074258105df5457c85282ca67ac3a4227f
patoattie <patoattie@gmail.com>
2019-10-21 19:06:12 -0300

    luchando

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,22 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 
+import { AngularFireModule } from "@angular/fire";
+import { AngularFirestoreModule } from "@angular/fire/firestore";
+import { AngularFireAuthModule } from "@angular/fire/auth";
+import { AngularFireStorageModule } from "@angular/fire/storage";
+import { environment } from '../environments/environment';
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFirestoreModule,
+    AngularFireAuthModule,
+    AngularFireStorageModule,
     AppRoutingModule
   ],
   providers: [],
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyAXFSL9K9c7oiG6X2oEhaQ0ho6au483G1c',
+    authDomain: 'parcial2bd.firebaseapp.com',
+    databaseURL: 'https://parcial2bd.firebaseio.com',
+    projectId: 'parcial2bd',
+    storageBucket: 'parcial2bd.appspot.com',
+    messagingSenderId: '673305199422',
+    appId: '1:673305199422:web:9c1827d7db58200633226f'
+  }
 };
 
 /*
