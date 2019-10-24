8d5cdd085d713519e2b0e44b97879982f722e47b
patoattie <patoattie@gmail.com>
2019-10-24 19:49:45 -0300

    haciendo tp

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,14 +5,30 @@ import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 
 import { AngularFireModule } from "@angular/fire";
-import { AngularFirestoreModule } from "@angular/fire/firestore";
+import { AngularFirestoreModule, FirestoreSettingsToken } from "@angular/fire/firestore";
 import { AngularFireAuthModule } from "@angular/fire/auth";
 import { AngularFireStorageModule } from "@angular/fire/storage";
 import { environment } from '../environments/environment';
+import { LoginComponent } from './componentes/login/login.component';
+import { FormsModule, ReactiveFormsModule, FormBuilder } from '@angular/forms';
+import { AuthService } from './servicios/auth.service';
+
+//primeNG
+import {ToolbarModule} from 'primeng/toolbar';
+import {ButtonModule} from 'primeng/button';
+import {CardModule} from 'primeng/card';
+import { InputTextModule } from "primeng/inputtext";
+import { PrincipalComponent } from './componentes/principal/principal.component';
+import { CabeceraComponent } from './componentes/cabecera/cabecera.component';
+import { RegistroComponent } from './componentes/registro/registro.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    PrincipalComponent,
+    CabeceraComponent,
+    RegistroComponent
   ],
   imports: [
     BrowserModule,
@@ -20,9 +36,19 @@ import { environment } from '../environments/environment';
     AngularFirestoreModule,
     AngularFireAuthModule,
     AngularFireStorageModule,
+    FormsModule,
+    ReactiveFormsModule,
+    ToolbarModule,
+    ButtonModule,
+    CardModule,
+    InputTextModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [
+    FormBuilder,
+    AuthService,
+    { provide: FirestoreSettingsToken, useValue: {} }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
